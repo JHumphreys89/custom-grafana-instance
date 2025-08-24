4baf01603f4b1ce2e044623bb492a578e8fe64dc
Jon Humphreys <115595085+JHumphreys89@users.noreply.github.com>
2025-08-23 22:09:39 -0400

    Running processes in background

--- bin/commands_to_run.sh
+++ bin/commands_to_run.sh
@@ -7,8 +7,8 @@ bin/grafana-server \
 cfg:default.paths.data="${GF_PATHS_DATA}" \
 cfg:default.paths.logs="${GF_PATHS_LOGS}" \
 cfg:default.paths.plugins="${GF_PATHS_PLUGINS}" \
-cfg:default.paths.provisioning="${GF_PATHS_PROVISIONING}"
+cfg:default.paths.provisioning="${GF_PATHS_PROVISIONING}" &
 
 # Command to run Prometheus when the container starts
 /usr/bin/prometheus \
---config.file "${GF_PATHS_CONFIG}/prometheus.yml"
+--config.file "${GF_PATHS_CONFIG}/prometheus.yml" &
