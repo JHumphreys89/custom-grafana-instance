57b88d164811266bc8d03891d6875280cee034bb
Jon Humphreys <115595085+JHumphreys89@users.noreply.github.com>
2025-08-23 22:17:58 -0400

    Testing new commands

--- bin/commands_to_run.sh
+++ bin/commands_to_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Command to run Grafana when the container starts
-bin/grafana-server \
+nohup bin/grafana-server \
 --homepath "${GF_INSTALL_DIR}" \
 --config "${GF_PATHS_CONFIG}" \
 cfg:default.paths.data="${GF_PATHS_DATA}" \
 cfg:default.paths.logs="${GF_PATHS_LOGS}" \
 cfg:default.paths.plugins="${GF_PATHS_PLUGINS}" \
-cfg:default.paths.provisioning="${GF_PATHS_PROVISIONING}" &
+cfg:default.paths.provisioning="${GF_PATHS_PROVISIONING}" >> /var/log/commands_to_run.log 2>&1 &
 
 # Command to run Prometheus when the container starts
-/usr/bin/prometheus \
---config.file "${GF_PATHS_CONFIG}/prometheus.yml" &
+nohup /usr/bin/prometheus \
+--config.file "${GF_PATHS_CONFIG}/prometheus.yml" >> /var/log/commands_to_run.log 2>&1  &
